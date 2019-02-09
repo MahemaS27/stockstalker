624dade6b8735a25221fab6f1e8f898155e8f830
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2019-02-09 14:51:56 -0500

    express runs react as well

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+const path = require('path');
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
 require('dotenv').config()
 
 const Alpaca = require('@alpacahq/alpaca-trade-api')
@@ -9,23 +15,20 @@ const alpaca = new Alpaca({
 })
 
 alpaca.getAccount().then((account) => {
-    console.log('Current Account:', account)
-  })
-
-  const express = require("express");
-const app = express();
-
-const port = process.env.PORT || 8080;
-
-app.get("/", (req, res) =>
-  res.send("yo")
-);
+  console.log('Current Account:', account)
+})
 
 app.get("/api/orders", (req,res) => {
   let allOrders = [];
   alpaca.getOrders({}).then(orders => {
     res.send(orders)
   })
+});
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(port, () => console.log("Listening on port " + port + "!"));
